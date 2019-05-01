28587187ce696184c1a3e9d59d570f787928af2b
yushusongmsazure <yushusong@outlook.com>
2019-04-30 22:01:43 -0700

    final version with tests passed

--- calculator.py
+++ calculator.py
@@ -3,62 +3,19 @@ Yushu Song
 Assignment 4
 """
 
-"""
-For your homework this week, you'll be creating a wsgi application of
-your own.
-
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
-
-  * Addition
-  * Subtractions
-  * Multiplication
-  * Division
-
-Your users should be able to send appropriate requests and get back
-proper responses. For example, if I open a browser to your wsgi
-application at `http://localhost:8080/multiple/3/5' then the response
-body in my browser should be `15`.
-
-Consider the following URL/Response body pairs as tests:
-
-```
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
-```
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
-
-"""
-
 import traceback
 
 def homepage():
     '''
     Display home page
     '''
-    body = '<h1>Here is how to use this page...</h1>'
-    # item_template = '<li>{operation}: {operation} two oprands given format {operation}/operand1/oprand2</li>'
-    # for op in ['Add', 'Multiply', 'Divide', 'Abstract']:
-    #     body.append(item_template.format(op))
-    # body.append('</ul>')
-    # return '\n'.join(body)
+    body = ['<h1>Here is how to use this page...</h1>']
+    body.append('<h2>Add:      http://localhost:8080/add/23/42      => 65</h2>')
+    body.append('<h2>Multiply: http://localhost:8080/add/12/5       => 60</h2>')
+    body.append('<h2>Divide:   http://localhost:8080/add/75/5       => 15</h2>')
+    body.append('<h2>subtract: http://localhost:8080/add/22/2       => 11</h2>')
 
-    return body
+    return '\n'.join(body)
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
@@ -67,30 +24,24 @@ def add(*args):
     return str(total)
 
 def multiply(*args):
-    """ Returns a STRING with the sum of the arguments """
+    """ Returns a STRING with the result of the arguments doing multiplication """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
     left_op = int(args[0])
     right_op = int(args[1])
 
     return str(left_op * right_op)
 
 def subtract(*args):
-    """ Returns a STRING with the sum of the arguments """
+    """ Returns a STRING with the result of the arguments doing subtraction """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
     left_op = int(args[0])
     right_op = int(args[1])
 
     return str(left_op - right_op)
 
 def divide(*args):
-    """ Returns a STRING with the sum of the arguments """
+    """ Returns a STRING with the division of the arguments """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
     left_op = int(args[0])
     right_op = int(args[1])
 
@@ -101,11 +52,6 @@ def resolve_path(path):
     Should return two values: a callable and an iterable of
     arguments.
     """
-
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
 
     funcs = {
         '': homepage,
@@ -127,13 +73,6 @@ def resolve_path(path):
     return func, args
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
     headers = [("Content-type", "text/html")]
     try:
         path = environ.get('PATH_INFO', None)
